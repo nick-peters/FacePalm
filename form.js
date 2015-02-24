2596e0443581c77e51037ae7b5be22b203373977
MATT RALSTON <jmatt.ralston@gmail.com>
2015-02-24 11:59:34 -0800

    added jQuery to append names for dropdown

--- form.js
+++ form.js
@@ -1,5 +1,23 @@
 $(function(){
 
+  var face = new Group('FacePalm');
+  var nick = new Member('Nick');
+  var naveed = new Member('Naveed');
+  var matt = new Member('Matt');
+  var lamson = new Member('Lamson');
+  var bucksnort = new Member('Adron');
+  face.addMembers(nick);
+  face.addMembers(naveed);
+  face.addMembers(matt);
+  face.addMembers(lamson);
+  console.log(face);
+
+
+//Appends name to dropdown menu for the selected group
+$('#nameChoice').append($('<option>').text(bucksnort.memberName).attr('value',bucksnort.memberName.toLowerCase()));
+
+
+
   function Group(groupName, members) {
     this.groupName = groupName;
     this.members = [];
